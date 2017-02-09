b185da735fa0fdf645f65e228a766e071988adf0
Student <Student>
2017-02-09 10:28:17 -0800

    Bug fixes denoted in comments

--- Program.cs
+++ Program.cs
@@ -5,18 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1 {
-    class Program {
-        static void Main(string[] args) {
-            double height;
+     class Program
+    {
+        static void Main(string[] args)
+        {
+             double height;
             double weight;
             double bmi;
             Console.WriteLine("Please enter your height in inches:");
 
-            // Error is in both readlines
-            height = Console.ReadLine();
+            //Fixed error using Double.Parse to cast input string to double for both height and weight
+            height = Double.Parse(Console.ReadLine());
 
             Console.WriteLine("Please enter your weight in pounds:");
-            weight = Console.ReadLine();
+            weight = Double.Parse(Console.ReadLine());
 
             Person p = new Person(height, weight);
             bmi = CalculateBMI(p.HeightInInches, p.WeightInPounds);
@@ -35,21 +37,21 @@ namespace ConsoleApplication1 {
 
             return bmi;
         }
-
     }
-
-
-
-    public class Person {
+    public class Person
+    {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public double HeightInInches { get; set; }
         public double WeightInPounds { get; set; }
 
-        //Error is in the constructor
-        public Person(double height, double weight) {
-            height = HeightInInches;
+        //Changed height formula to set HeightInInches variable to value of height argument.
+
+        public Person(double height, double weight)
+        {
+            HeightInInches = height;
             WeightInPounds = weight;
         }
     }
 }
+
