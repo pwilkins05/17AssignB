26d897dc61e423d9b61c0e6ce377380fd868733d
VirtualWesality <westheman81@gmail.com>
2017-02-12 16:04:27 -0800

    Source code that outputs the set of all subsets of a string you input

---
+++ stringsubsetcode.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void recPrintAllSubsets( string input, string output )
+{
+    int length = input.length();
+    if (length == 0)
+    {
+        cout << output << endl;
+                                    //BUG: Does not return anything
+    }
+    recPrintAllSubsets(input.substr(1, length-1), output);
+    recPrintAllSubsets(input.substr(1, length-1), input[0] + output);
+
+}
+
+int main()
+{
+    cout << "give me a string" << endl;
+                                            //Nowhere to receive a string input!
+    getline(cin, input);
+
+    cout << input << endl;
+
+                                            //Still needs to call the print function somehow
+
+    return 0;
+
+}
+
