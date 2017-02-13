586b9cf75526eca519d2fbe6a5e9d51a45fd0699
VirtualWesality <westheman81@gmail.com>
2017-02-12 23:37:02 -0800

    Fixed input bugs with Fibonacci code. clearly marked

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -2,20 +2,20 @@
 
 using namespace std;
 
-int getFib(float in);
+int getFib(int in);   //BUG: Input took non integer numbers
 
 //For the uninitiated:
 //The Fibonacci sequence is a sequence of numbers starting with 1, 1 where every following number is the sum of the last two.
 //The Fibonacci sequence should go 1, 1, 2, 3, 5, 8, 13, 21...
 
 //Please mark any bugs that have been fixed as such.
-//BUG: Output is incorrect.
+//BUG: Output is incorrect. (found and changed)
 //BUG: The input takes non-integer numbers.
 //TODO: Implement separate recursive implementation of getFib(). (Those familiar with recursion in C++ can have fun with this if they'd like.)
 
 int main()
 {
-    float in;
+    int in;  //BUG: Input took non-integer numbers. changed float to int
     int out;
 
     cout << "Hello! I can tell you any nth fibonacci number, calculating it by looping." << endl << "Which number do you want to know?" << endl;
@@ -25,7 +25,7 @@ int main()
     cout << endl << out;
 }
 
-int getFib(float in)
+int getFib(int in)  //BUG: Input took non-integer numbers.
 {
     int out = 1;
     int lastOut = 1;
